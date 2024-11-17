0449b719b22b206a51a8dfafa00b03f68475661e
TotalledZebra <Holappa57@gmail.com>
2024-11-17 14:30:28 +0200

    Include more data in DiffMining

--- src/DiffMining.py
+++ src/DiffMining.py
@@ -4,8 +4,6 @@ import os
 import logging
 import subprocess
 
-
-
 # Reset git repo at its main branch
 def reset_git_head(repo_path):
     try:
@@ -46,7 +44,7 @@ def find_repo_diff(repo_path, result_dir_path):
     
     try:
         # Check if path exists and if it's a git repo
-        if not os.path.exists(os.path.join(repo_path, '.git')):
+        if not os.path.exists(os.path.join(repo_path)):
             logging.error(f"No git repo found in {repo_path}")
             return
         
@@ -72,37 +70,74 @@ def find_repo_diff(repo_path, result_dir_path):
         
         for commit in all_commits:
             try:
+                parents = commit.parents
                 # Check for parent
-                if not commit.parents:
+                if not parents:
                     logging.debug(f"[Commit ignored (no parent)] : {commit.hash}")
                     continue
                 
                 # Get prievious commit hash
-                if hasattr(commit.parents[0], 'hash'):
-                    previous_commit_hash = commit.parents[0].hash
+                if hasattr(parents[0], 'hash'):
+                    previous_commit_hash = parents[0].hash
                 else:
                     # If parents[0] is a string, its the hash itself
-                    previous_commit_hash = commit.parents[0]
-                
+                    previous_commit_hash = parents[0]
+
                 modified_files = list(commit.modified_files)
                 
                 if not modified_files:
                     logging.debug(f"[Commit ignored (no touched file)] {commit.hash}")
                     continue
+
+                total_files_added = 0
+                total_files_deleted = 0
+                total_lines_added = 0
+                total_lines_deleted = 0
+                file_diffs = {}
+
+                # Find diff stats for each modified file and also add them to the total
+                for file in modified_files:
+
+                    diff = {}
+
+                    change_type = file.change_type.name
+
+                    if change_type == "ADD":
+                        total_files_added += 1
+                    elif change_type == "DELETE":
+                        total_files_deleted += 1
+
+                    added = 0
+                    removed = 0
+
+                    if diff_content := file.diff:
+
+                        # Same code as pydriller internally, just not doing the same work twice
+                        for line in diff_content.replace("\r", "").split("\n"):
+
+                            if line.startswith("-") and not line.startswith("---"):
+                                removed += 1
+                            elif line.startswith("+") and not line.startswith("+++"):
+                                added += 1
+      
+                        diff["diff_content"] = diff_content
+
+                    diff["lines_added"] = added
+                    diff["lines_deleted"] = removed
+                    total_lines_added += added
+                    total_lines_deleted += removed
+
+                    file_diffs[file.filename] = diff
                 
                 # Get diffs statistics
                 diff_stats = {
-                    'added': sum(1 for file in modified_files if file.change_type.name == 'ADD'),
-                    'deleted': sum(1 for file in modified_files if file.change_type.name == 'DELETE'),
+                    'files_added': total_files_added,
+                    'files_deleted': total_files_deleted,
+                    'lines_added': total_lines_added,
+                    'lines_deleted': total_lines_deleted,
                     'changed': len(modified_files),
-                    'files': [f.filename for f in modified_files]
+                    'file_diffs': file_diffs
                 }
-                
-                # Get diffs content
-                diff_content = ''
-                for modified_file in modified_files:
-                    if modified_file.diff:
-                        diff_content += modified_file.diff
                 
                 commit_info = {
                     'commit_hash': commit.hash,
@@ -110,8 +145,7 @@ def find_repo_diff(repo_path, result_dir_path):
                     'author': commit.author.name,
                     'date': commit.author_date.isoformat(),
                     'message': commit.msg,
-                    'diff_stats': diff_stats,
-                    'diff_content': diff_content
+                    'diff_stats': diff_stats
                 }
                 
                 commits_data.append(commit_info)
@@ -132,7 +166,7 @@ def find_repo_diff(repo_path, result_dir_path):
         # Save results
         output_file = os.path.join(result_dir_path, 'CommitsDiff.json')
         with open(output_file, 'w', encoding='utf-8') as json_file:
-            json.dump(commits_data, json_file, ensure_ascii=False, indent=4)
+            json.dump(commits_data, json_file)
         
     except Exception as e:
         logging.error(f"[Error analysing repo] : {repo_path} : {str(e)}")
@@ -151,9 +185,13 @@ def run():
         logging.error(f"Folder {repos_dir} doesn't exist")
         return
     
-    for project in os.listdir(repos_dir):
+    for project in ["ant.git"]:
         project_path = os.path.join(repos_dir, project)
         result_dir_path = os.path.join(results_dir, project)
+
+        # Skip projects that were already processed
+        if os.path.exists(os.path.join(result_dir_path, "CommitsDiff.json")):
+            continue
         
         if os.path.isdir(project_path):
             logging.info(f"\n[Analysing project] : {project}")
